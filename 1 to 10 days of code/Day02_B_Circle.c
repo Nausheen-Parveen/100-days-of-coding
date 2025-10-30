f68ddd145c9d36716cd34beceea3ffb9c1492a3e
NAUSHEEN PARVEEN <nausheenparveen940@gmail.com>
2025-10-31 02:47:04 +0530

    Update Day02_B_Circle.c

--- 1 to 10 days of code/Day02_B_Circle.c
+++ 1 to 10 days of code/Day02_B_Circle.c
@@ -1,4 +1,4 @@
-//Q4: Write a program to calculate the area and circumference of a circle given its radius.//
+//Q4: Write a program to calculate the area and circumference of a circle given its radius.
 #include <stdio.h>
 int main() {
     float radius, area, circumference;
@@ -12,3 +12,4 @@ int main() {
 
     return 0;
 }
+
