012fca5623a7b224058495237ce291f3f5f55251
NAUSHEEN PARVEEN <nausheenparveen940@gmail.com>
2025-10-31 02:47:33 +0530

    Update Day03_B_Swap.c

--- 1 to 10 days of code/Day03_B_Swap.c
+++ 1 to 10 days of code/Day03_B_Swap.c
@@ -1,4 +1,4 @@
-//Q6: Write a program to swap two numbers using a third variable.//
+//Q6: Write a program to swap two numbers using a third variable.
 #include <stdio.h>
 
 int main() {
@@ -15,3 +15,4 @@ int main() {
 
     return 0;
 }
+
